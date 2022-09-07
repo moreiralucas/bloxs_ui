7d49eeccf8a74924b8e82ce09de1edec749f50ad
Lucas Moreira <lucas-mopi@htomail.com>
2022-09-07 11:55:57 -0300

    Implements models

---
+++ src/services/models.ts
@@ -0,0 +1,17 @@
+export type TransactionModel = {
+    data_transacao: Date | string;
+    id_conta: number;
+    id_transacao: number;
+    valor: number;
+};
+
+export type AccountModel = {
+    data_criacao: Date | string;
+    flag_ativo: boolean;
+    id_conta: number;
+    id_pessoa: number;
+    limite_saque_diario: number;
+    saldo: number;
+    tipo_conta: number;
+};
+  
