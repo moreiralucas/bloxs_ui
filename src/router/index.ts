fd229fd34bacf607e3ef067c9c5458c3e7bde163
Lucas Moreira <lucas-mopi@htomail.com>
2022-09-08 01:13:58 -0300

    Remove dead code of services

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
-import RegisterView from '../views/RegisterView.vue'
+import CreateAccountView from '../views/CreateAccountView.vue'
 
 
 const routes: Array<RouteRecordRaw> = [
@@ -16,18 +16,10 @@ const routes: Array<RouteRecordRaw> = [
     component: LoginView
   },
   {
-    path: '/register',
-    name: 'register',
-    component: RegisterView
+    path: '/create_account',
+    name: 'create_account',
+    component: CreateAccountView
   },
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  // }
 ]
 
 const router = createRouter({
